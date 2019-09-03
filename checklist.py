e1926dceed2f7ff08e22500d3996860222447f56
FritzHeider <blueloke@gmail.com>
2019-09-03 08:55:31 -0700

    Having sertious problems still

--- checklist.py
+++ checklist.py
@@ -50,15 +50,57 @@ def mark_incomplete(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(str(index) + list_item)
+        #print(str(index) + list_item)
+        print("{} {}".format(index, list_item))
         index += 1
 
+def user_input(prompt):
+    #the input function displays the prompt
+    #and will wait for user_input
+    user_input = input(prompt)
+    return user_input
+
+
+def select(function_code):
+    if function_code == "C":
+        item = user_input("Name item you would like to create >")
+        create(item)
+        return True
+    elif function_code == "R":
+        index = user_input("Which index would you like to read? ")
+
+        print(read(index))
+        return True
+    elif function_code == "P":
+        list_all_items()
+        return True
+    elif function_code == "Q":
+        return False
+    else:
+        print("Unknown Option")
+    return True
 
 def test():
-    create("socks")
-    create("shoes")
-    list_all_items()
+    create("purple sox")
+    create("red cloak")
 
-test()
+    print(read(0))
+    print(read(1))
 
-print(checklist)
+    update(0, "purple socks")
+
+    destroy(
+
+    print(read(0))
+
+    #list_all_items()
+
+    #user_value = user_input("Please enter a value> ")
+    #print(user_value)
+
+running = True
+while running:
+        selection = user_input(
+            "Press C to add to the list, R to Read from the list and P to diplay the list"
+        )
+        select(selection)
